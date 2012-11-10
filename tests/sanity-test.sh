ad5f695d153a9b97510680f0a7455f4de3fee931
Hadi Bannazadeh <hadi.bannazadeh@utoronto.ca>
2012-11-10 18:06:58 -0500

    changing repos in stackrc

--- tests/sanity-test.sh
+++ tests/sanity-test.sh
@@ -1,4 +1,3 @@
-nova boot --flavor m1.tiny --image $OS_REGION_NAME-cirros-0.3.0-x86_64-uec --security_groups default --key-name key1 $OS_REGION_NAME-server$1
-#nova boot --flavor m1.tiny --image cirros-0.3.0-x86_64-uec --security_groups default --key-name key1 server$1
+nova boot --flavor m1.tiny --image $OS_REGION_NAME-cirros-0.3.0-x86_64-uec --security_groups default --key_name key1 $OS_REGION_NAME-server$1
 nova list
 sudo virsh list
