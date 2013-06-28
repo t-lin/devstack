b33dfb07d4ade1d2ac7a8c3c43e429f25381f786
Eliot J. Kang <joonmyung.kang@gmail.com>
2013-06-28 16:40:27 -0400

    Add init_janus to stack.sh for adding janus host and api to nova.conf

--- stack.sh
+++ stack.sh
@@ -2034,6 +2034,9 @@ if is_service_enabled whale; then
     init_whale
 fi
 
+if is_service_enabled janus; then
+    init_janus
+fi
 # All nova-compute workers need to know the vnc configuration options
 # These settings don't hurt anything if n-xvnc and n-novnc are disabled
 if is_service_enabled n-cpu; then
